f4ce3a3b9f13725b59957099f02ef14ec5279ccf
Nico Battaglia <nico-battaglia@hotmail.com>
2021-10-05 17:02:39 -0300

    Add graphical UI

--- logic.js
+++ logic.js
@@ -50,25 +50,36 @@ function play(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    let history = [];
-    let score = 0;
-    for (let index = 0; index<5; index++) {
-        let playerSelection = prompt("Your move: ").toLowerCase();
-        let computerSelection = computerPlay()
-        let result = play(playerSelection, computerSelection);
-        score = score + result;
-        history.push(result);
-        
-        console.log(`Your move: ${playerSelection}, Computer: ${computerSelection}`)
-        if (result=== 0) {console.log("Draw")}
-        if (result === 1) { console.log("Win") }
-        if (result === -1) { console.log("Lose") }
-    }
+let history = [];
+let score = 0;
+function game(playerSelection) {
+    let computerSelection = computerPlay()
+    let result = play(playerSelection, computerSelection);
+    score = score + result;
+    history.push(result);
+
+    pHumanMove.innerText=`Your move: ${playerSelection}`;
+    pComputerMove.innerText=`Computer: ${computerSelection}`
     
-    console.table({resultados: history})
-    console.log("score: " + score);
-    if (score>0) {console.log("You win!")}
-    if (score < 0) { console.log("You lose!") }
-    if (score === 0) { console.log("Draw") }
+    if (result=== 0) {pResult.innerText="Draw"}
+    if (result === 1) { pResult.innerText="Win"}
+    if (result === -1) { pResult.innerText="Lose"}
+
+    pHistory.innerText = `Puntaje acumulado: ${score}`;
 }
+
+//==========================================================================
+// Event Listeners
+//==========================================================================
+
+const buttons = document.querySelectorAll("button");
+const pHumanMove = document.querySelector("#humanMove");
+const pComputerMove = document.querySelector("#computerMove");
+const pResult = document.querySelector("#result");
+const pHistory = document.querySelector("#history");
+
+buttons.forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
+        game(e.target.id);
+    })
+})
